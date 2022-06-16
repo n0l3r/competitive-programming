a56ebf8b15fed5ff4e9a476ee7392587dcce1e48
n0l3r <naufal.120140044@student.itera.ac.id>
2022-06-16 18:19:42 +0700

    48

--- leetcode/rotate-image.cpp
+++ leetcode/rotate-image.cpp
@@ -13,3 +13,20 @@ void rotate(vector<vector<int>> &matrix) {
         reverse(matrix[i].begin(), matrix[i].end());
     }
 }
+
+int main(){
+    vector<vector<int>> matrix = {
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12},
+        {13,14,15,16}
+    };
+    rotate(matrix);
+    for(int i = 0; i < matrix.size(); i++){
+        for(int j = 0; j < matrix[i].size(); j++){
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
