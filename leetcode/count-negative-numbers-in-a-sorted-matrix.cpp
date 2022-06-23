70b28a4aa541f477398110f729f60b88cd880e9f
n0l3r <naufal.120140044@student.itera.ac.id>
2022-06-23 10:19:24 +0700

    1351

---
+++ leetcode/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countNegative(vector<vector<int>>& grid) {
+    int m = grid.size();
+    int n = grid[0].size();
+    int count = 0;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j] < 0) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
